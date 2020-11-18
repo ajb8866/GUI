775f34d4b95e65bae94281ea4d7e6a7a357acaf2
Averi Bates <Averi.J.Bates@ou.edu>
2020-11-18 16:16:16 -0600

    First Commit

---
+++ src/GUITest1.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class GUITest1 {
+
+	public static void main(String[] args) {
+		final int W  = 400;
+		final int H  = 666;
+		// TODO Auto-generated method stub
+		//all program sequntitial up until now
+		// END
+		JFrame frame = new JFrame("Dark Wave");
+		frame.setSize(W, H);
+		frame.setLocationRelativeTo(null);
+		
+	}
+
+}
